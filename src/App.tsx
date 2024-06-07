f6bd7bafea22f3df7dd98f24e09a9f033432c9ca
Kevin Martinez <kkevinmartinez7@gmail.com>
2024-06-06 22:47:40 -0600

    Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './presentation/router/router'
 
 export const App = () => {
+
+  useEffect(() => {
+    document.documentElement.classList.add("dark");
+  }, [])
+  
   return (
     <RouterProvider router={router} />
   )
