11a5fa404f77fe9186a7172f735211a958301e5f
Kevin Martinez <kkevinmartinez7@gmail.com>
2024-06-07 11:18:48 -0600

    Chat Messages Components

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { MyMessage, GptMessage } from '../../components';
 
 const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+
+          {/* Welcome */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayuda con las correcciones" />
+          <MyMessage text="Hola Mundo"/>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
