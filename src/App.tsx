90395204f95c2b66296753b3d017b6010599a50d
Kevin Martinez <kkevinmartinez7@gmail.com>
2024-06-06 22:54:12 -0600

    Sidebar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './presentation/router/router'
 
 export const App = () => {
 
-  useEffect(() => {
-    document.documentElement.classList.add("dark");
-  }, [])
-  
   return (
     <RouterProvider router={router} />
   )
--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -1,5 +1,6 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { menuRoutes } from "../router/router";
+import { SidebarMenuItem } from "../components";
 
 export const DashboardLayout = () => {
   return (
@@ -13,25 +14,7 @@ export const DashboardLayout = () => {
         <div className="border-gray-700 border my-3" />
 
         {menuRoutes.map((option) => (
-            <NavLink
-            key={option.to}
-            to={option.to}
-            className={({ isActive }) =>
-              isActive
-                ? "flex justify-center items-center bg-gray-800 rounded-md p-2 transition-colors"
-                : "flex justify-center items-center :hover:bg-gray-800 rounded-md p-2 transition-colors"
-            }
-          >
-            <i className={`${option.icon} text-2xl mr-4 text-indigo-400`}></i>
-            <div className="flex flex-col flex-grow">
-                <span className="text-white text-lg font-semibold">
-                    {option.title}
-                </span>
-                <span className="text-gray-400 text-sm">
-                    {option.description}
-                </span>
-            </div>
-          </NavLink>
+            <SidebarMenuItem key={option.to} {...option}/>
         ))}
       </nav>
 
